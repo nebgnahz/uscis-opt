f7776361e66d613cd4c6ccf4e55a2e0409ff2fd1
Ben Zhang <benzh@cs.berkeley.edu>
2018-08-04 00:35:24 -0700

    only https requests

--- src/schema.rs
+++
@@ -1,9 +0,0 @@
-table! {
-    records (id) {
-        id -> Unsigned<Bigint>,
-        title -> Varchar,
-        description -> Text,
-        update_date -> Date,
-        crawl_time -> Datetime,
-    }
-}
